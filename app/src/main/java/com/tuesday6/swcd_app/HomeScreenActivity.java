88b53104bad449179aeb52f2982016a6a4e588fc
cfair22 <cfair22@gmail.com>
2015-03-07 16:57:40 -0500

    Got search to run, however the search.php returns null values so im not sure which is wrong the listResult.java or search.php
    will do more testing

--- app/src/main/java/com/tuesday6/swcd_app/HomeScreenActivity.java
+++ app/src/main/java/com/tuesday6/swcd_app/HomeScreenActivity.java
@@ -1,13 +1,17 @@
 package com.tuesday6.swcd_app;
 
+import android.app.ActionBar;
+import android.app.Activity;
 import android.app.ListActivity;
 import android.app.ProgressDialog;
 import android.app.SearchManager;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.DataSetObserver;
 import android.os.AsyncTask;
 import android.os.Build;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -18,6 +22,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
@@ -35,193 +41,35 @@ import java.util.HashMap;
 import java.util.List;
 
 
-public class HomeScreenActivity extends ActionBarActivity {
+public class HomeScreenActivity extends Activity implements View.OnClickListener {
 
-    private ListView listView;
-    private SearchView searchView;
+    private Button searchButton;
+    private EditText editSearch;
 
-    // Progress Dialog
-    private ProgressDialog pDialog;
-
-    // Creating JSON Parser object
-    JSONParser jParser = new JSONParser();
-
-    ArrayList<HashMap<String, String>> stainsList;
-
-    // url to get the idiom list
-    private static String url_search = "http://southwestcd.com/search.php";
-
-    // JSON Node names
-    private static final String TAG_SUCCESS = "success";
-    private static final String TAG_STAINS = "stains";
-    private static final String TAG_STAIN_NAME_ID = "stain_name_id";
-    private static final String TAG_STAIN_ID = "stain_id";
-
-
-    // products JSONArray
-    JSONArray stains = null;
-
-    //search key value
-    public String searchkey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
-        // Get listview
-        listView = (ListView) findViewById(R.id.search_list_view);
-        searchView = (SearchView) findViewById(R.id.search);
-
-        Intent myIntent = getIntent();
-
-        // gets the arguments from previously created intent
-        searchkey = myIntent.getStringExtra("keyword");
-
-        // Hashmap for ListView
-        stainsList = new ArrayList<HashMap<String, String>>();
-
-        // Loading idioms in Background Thread
-        new LoadStains().execute();
-
-
-
-
-
-        // on selecting single idioms
-        // to do something
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                // getting values from selected ListItem
-                String iid = ((TextView) view.findViewById(R.id.stain_id)).getText()
-                        .toString();
-
-            }
-        });
+        editSearch = (EditText)findViewById(R.id.edit_search);
+        searchButton = (Button) findViewById(R.id.search_button);
+        searchButton.setOnClickListener(this);
     }
 
-
-    /**
-     * Background Async Task to Load Idioms by making HTTP Request
-     * */
-    class LoadStains extends AsyncTask<String, String, String> {
-
-        /**
-         * Before starting background thread Show Progress Dialog
-         * */
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            pDialog = new ProgressDialog(HomeScreenActivity.this);
-            pDialog.setMessage("Loading Stains. Please wait...");
-            pDialog.setIndeterminate(false);
-            pDialog.setCancelable(false);
-            pDialog.show();
+    @Override
+    public void onClick(View v){
+        if (v.getId() == R.id.search_button){
+            Intent searchIntent = new Intent(HomeScreenActivity.this, ListResult.class);
+            searchIntent.putExtra("key", editSearch.getText().toString());
+            startActivity(searchIntent);
         }
-
-        /**
-         * getting Idioms from url
-         * */
-        protected String doInBackground(String... args) {
-            // Building Parameters
-            List<NameValuePair> params = new ArrayList<NameValuePair>();
-
-            //value captured from previous intent
-            params.add(new BasicNameValuePair("keyword", searchkey));
-
-            // getting JSON string from URL
-            JSONObject json = jParser.makeHttpRequest(url_search, "GET", params);
-
-            // Check your log cat for JSON response
-            Log.d("Search Stains: ", json.toString());
-
-            try {
-                // Checking for SUCCESS TAG
-                int success = json.getInt(TAG_SUCCESS);
-
-                if (success == 1) {
-                    // products found
-                    // Getting Array of Products
-                    stains = json.getJSONArray(TAG_STAINS);
-
-                    // looping through All Products
-                    for (int i = 0; i < stains.length(); i++) {
-                        JSONObject c = stains.getJSONObject(i);
-
-                        // Storing each json item in variable
-                        String id = c.getString(TAG_STAIN_ID);
-                        String stain_name = c.getString(TAG_STAIN_NAME_ID);
-
-                        // creating new HashMap
-                        HashMap<String, String> map = new HashMap<String, String>();
-
-                        // adding each child node to HashMap key => value
-                        map.put(TAG_STAIN_ID, id);
-                        map.put(TAG_STAIN_NAME_ID, stain_name);
-
-                        // adding HashList to ArrayList
-                        stainsList.add(map);
-                    }
-                } else {
-                    // no idioms found
-                    //do something
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-            //return "success";
-            return null;
-        }
-
-        /**
-         * After completing background task Dismiss the progress dialog
-         * **/
-        protected void onPostExecute(String file_url) {
-            // dismiss the dialog after getting the related idioms
-            if (pDialog != null) {
-                pDialog.dismiss();
-                pDialog = null;
-            }
-
-            // updating UI from Background Thread
-            runOnUiThread(new Runnable() {
-                public void run() {
-                    /**
-                    * Updating parsed JSON data into ListView
-                    * */
-                    ListAdapter adapter = new SimpleAdapter(
-                            HomeScreenActivity.this, stainsList,
-                            R.layout.activity_home_screen, new String[]{TAG_STAIN_ID, TAG_STAIN_NAME_ID},
-                            new int[]{R.id.stain_id, R.id.stain_name_id});
-
-                    // updating listview
-                    listView.setAdapter(adapter);
-                }
-            });
-
-        }
-
     }
-
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_home_screen, menu);
-
-
-        // Get the SearchView and set the searchable configuration
-        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
-        // Assumes current activity is the searchable activity
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-        searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
 
         return true;
     }
---
+++ app/src/main/java/com/tuesday6/swcd_app/ListResult.java
@@ -0,0 +1,164 @@
+package com.tuesday6.swcd_app;
+
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+
+public class ListResult extends ListActivity {
+
+    // Progress Dialog
+    private ProgressDialog pDialog;
+
+    // Creating JSON Parser object
+    JSONParser jParser = new JSONParser();
+
+    ArrayList<HashMap<String, String>> stainsList;
+
+    // url to get the idiom list
+    private static String url_search = "http://southwestcd.com/search.php";
+
+    // JSON Node names
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_STAINS = "stains";
+    private static final String TAG_STAIN_NAME_ID = "stain_name_id";
+    private static final String TAG_STAIN_ID = "stain_id";
+
+    // products JSONArray
+    JSONArray stains = null;
+    //search key value
+    public String searchkey;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_result);
+        Intent myIntent = getIntent();
+
+        searchkey = myIntent.getStringExtra("keyword");
+
+        stainsList = new ArrayList<HashMap<String, String>>();
+
+        new LoadStains().execute();
+
+        ListView lv = getListView();
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String iid = ((TextView) view.findViewById(R.id.stain_id)).getText().toString();
+            }
+        });
+    }
+
+    class LoadStains extends AsyncTask<String, String, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(ListResult.this);
+            pDialog.setMessage("Loading Stains. Please wait...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... args) {
+            // Building Parameters
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+
+            //value captured from previous intent
+            params.add(new BasicNameValuePair("keyword", searchkey));
+
+            // getting JSON string from URL
+            JSONObject json = jParser.makeHttpRequest(url_search, "GET", params);
+
+            // Check your log cat for JSON response
+            Log.d("Search Stains: ", json.toString());
+
+            try {
+                // Checking for SUCCESS TAG
+                int success = json.getInt(TAG_SUCCESS);
+
+                if (success == 1) {
+                    // products found
+                    // Getting Array of Products
+                    stains = json.getJSONArray(TAG_STAINS);
+
+                    // looping through All Products
+                    for (int i = 0; i < stains.length(); i++) {
+                        JSONObject c = stains.getJSONObject(i);
+
+                        // Storing each json item in variable
+                        String id = c.getString(TAG_STAIN_ID);
+                        String stain_name = c.getString(TAG_STAIN_NAME_ID);
+
+                        // creating new HashMap
+                        HashMap<String, String> map = new HashMap<String, String>();
+
+                        // adding each child node to HashMap key => value
+                        map.put(TAG_STAIN_ID, id);
+                        map.put(TAG_STAIN_NAME_ID, stain_name);
+
+                        // adding HashList to ArrayList
+                        stainsList.add(map);
+                    }
+                } else {
+                    // no idioms found
+                    //do something
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            //return "success";
+            return null;
+        }
+    }
+    /**
+     * After completing background task Dismiss the progress dialog
+     * **/
+    protected void onPostExecute(String file_url) {
+        // dismiss the dialog after getting the related idioms
+        pDialog.dismiss();
+        // updating UI from Background Thread
+        runOnUiThread(new Runnable() {
+            public void run() {
+            /**
+            * Updating parsed JSON data into ListView
+            * */
+                ListAdapter adapter;
+                adapter = new SimpleAdapter(
+                        ListResult.this, stainsList,
+                        R.layout.list_view, new String[]{TAG_STAIN_ID, TAG_STAIN_NAME_ID},
+                        new int[]{R.id.stain_id, R.id.stain_name_id});
+
+                // updating listview
+                setListAdapter(adapter);
+            }
+        });
+
+    }
+}
