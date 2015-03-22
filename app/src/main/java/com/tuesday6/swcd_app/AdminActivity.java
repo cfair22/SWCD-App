18644b39a88f92475c07196c69af277d818c5514
cfair22 <cfair22@gmail.com>
2015-03-22 14:47:56 -0400

    Configured the login to send to adminActivity. Also added functionality to AdminActivity to send data to database Then the intention is to once successfully added to database have the steps for single stain appear with newly added stains.
    Need to change return_stain.php to accept the stain_name instead of stain_id.

--- app/src/main/java/com/tuesday6/swcd_app/AdminActivity.java
+++ app/src/main/java/com/tuesday6/swcd_app/AdminActivity.java
@@ -1,20 +1,137 @@
 package com.tuesday6.swcd_app;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class AdminActivity extends ActionBarActivity {
+public class AdminActivity extends Activity implements View.OnClickListener {
+
+    JSONParser jsonParser = new JSONParser();
+
+    ProgressDialog progressDialog;
+    EditText newStainName;
+    EditText newCarpetHowto;
+    EditText newCarpetNotes;
+    EditText newTileHowto;
+    EditText newTileNotes;
+    EditText newRugHowto;
+    EditText newRugNotes;
+    EditText newUpholsteryHowto;
+    EditText newUpholsteryNotes;
+    Button newStain;
+
+    //url to create new stain
+    private static String urlNewStain = "http://southwestcd.com/add_stain.php";
+
+    //JSON node names
+    private static final String TAG_SUCCESS = "success";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        newStainName = (EditText) findViewById(R.id.admin_stain_name);
+        newCarpetHowto = (EditText)findViewById(R.id.admin_carpet_howto);
+        newCarpetNotes = (EditText) findViewById(R.id.admin_carpet_notes);
+        newTileHowto = (EditText) findViewById(R.id.admin_tile_howto);
+        newTileNotes = (EditText) findViewById(R.id.admin_tile_notes);
+        newRugHowto = (EditText) findViewById(R.id.admin_rugs_howto);
+        newRugNotes = (EditText) findViewById(R.id.admin_rugs_notes);
+        newUpholsteryHowto = (EditText) findViewById(R.id.admin_upholstery_howto);
+        newUpholsteryNotes = (EditText) findViewById(R.id.admin_upholstery_notes);
+
+        newStain = (Button) findViewById(R.id.submit_stain);
+        newStain.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v){
+        if(v.getId() == R.id.submit_stain){
+            new AddStain().execute();
+        }
+    }
 
+    class AddStain extends AsyncTask<String, String, String>{
+        protected void onPreExecute(){
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(AdminActivity.this);
+            progressDialog.setMessage("Adding New Stain");
+            progressDialog.setIndeterminate(false);
+            progressDialog.setCancelable(true);
+            progressDialog.show();
+        }
+
+        protected String doInBackground(String... args){
+            String stain_name_db = newStainName.getText().toString();
+            String carpet_howto_db = newCarpetHowto.getText().toString();
+            String carpet_notes_db = newCarpetNotes.getText().toString();
+            String tile_howto_db = newTileHowto.getText().toString();
+            String tile_notes_db = newTileNotes.getText().toString();
+            String area_rugs_howto_db = newRugHowto.getText().toString();
+            String area_rugs_notes_db = newRugNotes.getText().toString();
+            String upholstery_howto_db = newUpholsteryHowto.getText().toString();
+            String upholstery_notes_db = newUpholsteryNotes.getText().toString();
+
+            //Building Parameters
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            params.add(new BasicNameValuePair("stain_name",stain_name_db ));
+            params.add(new BasicNameValuePair("carpet_howto_db", carpet_howto_db));
+            params.add(new BasicNameValuePair("carpet_notes_db", carpet_notes_db));
+            params.add(new BasicNameValuePair("tile_howto_db", tile_howto_db));
+            params.add(new BasicNameValuePair("tile_notes_db", tile_notes_db));
+            params.add(new BasicNameValuePair("area_rugs_howto_db", area_rugs_howto_db));
+            params.add(new BasicNameValuePair("area_rugs_notes_db", area_rugs_notes_db));
+            params.add(new BasicNameValuePair("upholstery_howto_db", upholstery_howto_db));
+            params.add(new BasicNameValuePair("upholstery_notes_db", upholstery_notes_db));
+
+            //getting JSON object
+            JSONObject jsonObject = jsonParser.makeHttpRequest(urlNewStain, "POST", params);
+
+            Log.d("Create Stain", jsonObject.toString());
+
+            //check for success tag
+            try{
+                int success = jsonObject.getInt(TAG_SUCCESS);
+
+                if(success == 1){
+                    Intent intent = new Intent(AdminActivity.this, StepsForStainActivity.class);
+                    intent.putExtra("stain_name_db", newStainName.getText().toString());
+                    startActivity(intent);
+                } else {
+                    System.out.println("Failed to create stain");
+                }
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        protected void onPostExecute(String file_url){
+            if (progressDialog != null) {
+                progressDialog.dismiss();
+                progressDialog = null;
+            }
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
--- app/src/main/java/com/tuesday6/swcd_app/Login.java
+++ app/src/main/java/com/tuesday6/swcd_app/Login.java
@@ -1,19 +1,134 @@
 package com.tuesday6.swcd_app;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.sql.BatchUpdateException;
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class Login extends ActionBarActivity {
+public class Login extends Activity implements View.OnClickListener {
+
+    private Button loginButton;
+    private EditText passwordEditText;
+
+    //progress Dialog
+    private ProgressDialog progressDialog;
+
+    //JSON parser class
+    JSONParser jsonParser = new JSONParser();
+
+    //url to login.php
+    private static final String urlLogin = "http://southwestcd.com/login.php";
+
+    //JSON names
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_PASSWORD = "password";
+    private static final String TAG_MESSAGE = "message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        passwordEditText = (EditText) findViewById(R.id.admin_login_editText);
+
+        loginButton = (Button)findViewById(R.id.admin_login_at_login);
+        loginButton.setOnClickListener(this);
+
+
     }
 
+    @Override
+    public void onClick(View v){
+        if (v.getId() == R.id.admin_login_at_login){
+            new AdminLogin().execute();
+        }
+    }
+
+
+    class AdminLogin extends AsyncTask<String, String, String>{
+
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(Login.this);
+            progressDialog.setMessage("Attempting Login..");
+            progressDialog.setIndeterminate(false);
+            progressDialog.setCancelable(true);
+            progressDialog.show();
+        }
+
+        protected String doInBackground(String... args){
+            //Check for success tag
+            int success;
+            String password = passwordEditText.getText().toString();
+
+
+            try{
+                //Building parameteres
+                List<NameValuePair> params = new ArrayList<NameValuePair>();
+                params.add(new BasicNameValuePair("password", password));
+
+                Log.d("Attempt Login...", jsonParser.toString());
+
+                JSONObject jsonObject = jsonParser.makeHttpRequest(urlLogin, "GET", params);
+
+                System.out.println("Json parser has made http request");
+
+                Log.d("Login Attempt", jsonObject.toString());
+                success = jsonObject.getInt(TAG_SUCCESS);
+                if (success == 1){
+                    System.out.println("Login was sucessful");
+                    Log.d("Login Sucessful", jsonObject.toString());
+
+                    //Intent to send password
+                    Intent intent = new Intent(Login.this, AdminActivity.class);
+                    startActivity(intent);
+                } else{
+                   System.out.println("Attempt was not successful");
+                   progressDialog.dismiss();
+
+
+                }
+
+            } catch (JSONException e){
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
+
+    protected void onPostExecute(String file_url){
+        //loginMessage.setText("HEllo WOrld");
+
+        if (progressDialog != null){
+            progressDialog.dismiss();
+            progressDialog = null;
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
